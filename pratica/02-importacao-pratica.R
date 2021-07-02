66a81d7f309dee2e0647e8257065de528e6f37e5
Beatriz Milz <beatriz.milz@hotmail.com>
2021-07-01 22:24:48 -0300

    adiciona pratica

---
+++ pratica/02-importacao-pratica.R
@@ -0,0 +1,97 @@
+# -------------------------------------------------------------------------
+
+# Prática 1 : Importar a base do imdb separada por ano
+
+# Carregar o pacote readr
+library(readr)
+
+# importar uma base
+imdb_1916 <- read_rds("dados/por_ano/imdb_1916.rds")
+
+# obtendo um vetor com o caminho para  todos os arquivos
+arquivos <- list.files("dados/por_ano", full.names = TRUE, pattern = ".rds")
+
+# ver o conteúdo de arquivos
+arquivos
+
+# aplicar a função read_rds para cada elemento do
+# vetor arquivos, e unindo em um data.frame
+imdb <- purrr::map_dfr(arquivos, read_rds)
+
+# ver o conteudo de imdb: conseguimos a base completa!
+imdb
+
+##  duvida --- importar só parte dos arquivos
+arquivos[1:9]
+filmes_pre_guerra <- purrr::map_dfr(arquivos[1:9], read_rds)
+
+## duvida 2 --- importar duas bases e empilhar
+# importar as bases
+imdb_1916 <- read_rds("dados/por_ano/imdb_1916.rds")
+imdb_1920 <- read_rds("dados/por_ano/imdb_1920.rds")
+
+# empilhar as bases
+dplyr::bind_rows(imdb_1916, imdb_1920)
+
+
+# -------------------------------------------------------------------------
+
+# Prática 2
+# importar imdb_nao_estruturada.xlsx
+
+# carregar o pacote readxl
+library(readxl)
+
+# verificar quais sheets/abas tem em uma base
+excel_sheets("dados/imdb_nao_estruturada.xlsx")
+
+# importar a aba "Sheet1" para obter o nome das colunas
+nomes_colunas <- read_excel("dados/imdb_nao_estruturada.xlsx",
+                            sheet = "Sheet1")
+nomes_colunas$nome
+
+# Importar usando a readxl
+imdb_nao_estruturada <- read_excel(
+  # caminho até a base
+  path = "dados/imdb_nao_estruturada.xlsx",
+  # número de linhas para pular
+  skip = 3,
+  # usar o vetor nomes_coluna$nome para nomear as colunas
+  col_names = nomes_colunas$nome,
+  # número maximo de linhas
+  n_max = 3714
+)
+
+
+# Fazer a importação usando a ferramenta Import Dataset
+# do RStudio
+
+# Carregar o pacote readxl
+library(readxl)
+#
+imdb_nao_estruturada_import <- read_excel(
+  # caminho até a base
+  "dados/imdb_nao_estruturada.xlsx",
+  # não usar a primeira linha para nomear as colunas
+  col_names = FALSE,
+  # número de linhas para pular
+  skip = 3
+)
+
+# ver o resultado
+imdb_nao_estruturada_import
+
+# daqui em diante teremos que "limpar" sem o import dataset
+
+# Usar o vetor nomes_colunas$nome para nomear as colunas
+# da base
+names(imdb_nao_estruturada_import) <- nomes_colunas$nome
+
+# extra! ---
+# remover a última linha
+
+# contar quantas linhas tem:
+n_linhas <- nrow(imdb_nao_estruturada_import)
+
+# remover ultima linha
+imdb <- imdb_nao_estruturada_import[-n_linhas, ]
--- scripts/02-importacao.R
+++ scripts/02-importacao.R
@@ -1,3 +1,5 @@
+## install.packages("tidyverse")
+
 library(tidyverse)
 
 # Caminhos até o arquivo --------------------------------------------------
@@ -8,18 +10,30 @@ library(tidyverse)
 # Caminhos relativos
 "dados/imdb.csv"
 
-# (cara(o) professora(o), favor lembrar de falar da dica 
+# (cara(o) professora(o), favor lembrar de falar da dica
 # de navegação entre as aspas)
+
+"dados/imdb.csv"
 
 # Tibbles -----------------------------------------------------------------
 
 airquality
+class(airquality)
+as_tibble(airquality)
 class(as_tibble(airquality))
 
 # Lendo arquivos de texto -------------------------------------------------
 
+imdb_csv <- read_csv("dados/imdb.csv")
+
 # CSV, separado por vírgula
 imdb_csv <- read_csv(file = "dados/imdb.csv")
+
+glimpse(imdb_csv)
+
+# ERRO!! O arquivo é separado por ponto e virgula, e a função
+# espera a separacao por virgula
+View(read_csv("dados/imdb2.csv"))
 
 # CSV, separado por ponto-e-vírgula
 imdb_csv2 <- read_csv2(file = "dados/imdb2.csv")
@@ -29,30 +43,40 @@ imdb_txt <- read_delim(file = "dados/imdb.txt", delim = "\t")
 
 # A função read_delim funciona para qualquer tipo de separador
 imdb_delim <- read_delim("dados/imdb.csv", delim = ",")
-imdb_delim <- read_delim("dados/imdb2.csv", delim = ";")
+imdb_delim2 <- read_delim("dados/imdb2.csv", delim = ";")
 
 # direto da internet
-imdb_csv <- read_csv("https://raw.githubusercontent.com/curso-r/202005-r4ds-1/master/dados/imdb.csv")
+imdb_csv_url <- read_csv("https://raw.githubusercontent.com/curso-r/202005-r4ds-1/master/dados/imdb.csv")
 
 # Interface point and click do RStudio também é útil!
 
-# Lendo arquivos do Excel -------------------------------------------------
+library(readr)
+imdb2 <- read_delim("dados/imdb2.csv", ";",
+                    escape_double = FALSE, trim_ws = TRUE)
+View(imdb2)
 
+# Lendo arquivos do Excel -------------------------------------------------
+# install.packages("readxl")
 library(readxl)
 
 imdb_excel <- read_excel("dados/imdb.xlsx")
 excel_sheets("dados/imdb.xlsx")
 
+?read_excel()
+
+read_excel("dados/imdb.xlsx", sheet = "Sheet1")
+
 # Salvando dados ----------------------------------------------------------
 
 # As funções iniciam com 'write'
 
+
 # CSV
-write_csv(imdb, file = "imdb.csv")
+write_csv(imdb_csv, file = "imdb.csv")
 
 # Excel
 library(writexl)
-write_xlsx(imdb, path = "imdb.xlsx")
+write_xlsx(imdb_excel, path = "imdb.xlsx")
 
 # O formato rds -----------------------------------------------------------
 
@@ -60,4 +84,31 @@ write_xlsx(imdb, path = "imdb.xlsx")
 # Você pode salvar qualquer objeto do R em formato .rds
 
 imdb_rds <- readr::read_rds("dados/imdb.rds")
+# equivalente do base: readRDS()
+
 readr::write_rds(imdb_rds, file = "dados/imdb_rds.rds")
+# equivalente do base:  saveRDS()
+
+# função do base
+# Função diferente:   ?save() - é para salvar o .RData
+
+
+# EXTRA!
+# importando dados do google sheets -----------
+
+url <- "https://docs.google.com/spreadsheets/d/1y3oRSspAKQzLyAx2F3cpj89RiTI88wVf-hYZb4oRiuA/edit?usp=sharing"
+
+# install.packages("googlesheets4")
+library(googlesheets4)
+library(janitor)
+
+dados_formulario <- read_sheet(url)
+
+# dados_formulario$`Em qual estado você mora?`
+
+#install.packages("janitor")
+
+dados_limpo <- clean_names(dados_formulario)
+
+# dados_limpo$em_qual_estado_voce_mora
+
